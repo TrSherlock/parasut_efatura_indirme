010263ebfb69f5834e3b94017ceaaa9db29bb1a3
TrSherlock <33941709+TrSherlock@users.noreply.github.com>
2023-02-25 02:07:44 +0300

    Update bekleyen_efaturalar.js

--- bekleyen_efaturalar.js
+++ bekleyen_efaturalar.js
@@ -6,5 +6,5 @@ fetch("https://uygulama.parasut.com/302587/e_invoices")
 .then(f => {
     f.e_invoices
     .filter(inv=>new Date(inv.issue_date).getFullYear() == $year && new Date(inv.issue_date).getMonth() == $month)
-    .forEach(ubl=>console.log("https://uygulama.parasut.com"+ubl.signed_ubl_url))
+    .forEach((ubl, i)=>console.log(i, "https://uygulama.parasut.com"+ubl.signed_ubl_url))
 })
